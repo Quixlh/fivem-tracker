0af49c7f7ee4d003069476ac4f403f7683926309
Quix <81916786+Quixlh@users.noreply.github.com>
2025-01-26 23:53:55 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,18 @@
-shared_script '@WaveShield/resource/waveshield.lua' --this line was automatically written by WaveShield
-
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
+author "Quix"
+
 shared_scripts {
     "@es_extended/imports.lua",
-    "clientloader.lua",
     "config.lua"
 }
 
-server_script "server/server.lua"
+client_scripts {
+    "client/client.lua"
+}
+
+server_scripts {
+    "server/server.lua"
+}
